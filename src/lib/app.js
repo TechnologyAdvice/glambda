578eb196b91ab2a7a3d7b978d0fe16936e34ec3f
fluidbyte <kent@fluidbyte.net>
2015-07-29 11:46:05 -0400

    Use Bristol for logging, cleanup unused deps

--- src/lib/app.js
+++ src/lib/app.js
@@ -3,6 +3,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fork = require('child_process').fork
 const path = require('path')
+const log = require('bristol')
+
+// Setup logs
+log.addTarget('console')
 
 const runner = path.resolve('build/lib/runner')
 
@@ -15,17 +19,11 @@ const port = process.env.PORT || 8181
  * Handles response from forked lambda runner procs
  * @param {Object} msg Message object from child proc
  * @param {Object} res Express response object
+ * @param {String} output Output destination
  */
-const procResponse = (msg, res) => {
-  switch (msg.type) {
-    case 'success':
-      res.status(200).send(msg.output)
-      break
-    case 'error':
-      res.status(500).send(msg.output)
-      break
-    case 'metric':
-      console.log(msg.output)
+const procResponse = (msg, res, output) => {
+  if (output === 'console') {
+    log.info(msg.output)
   }
 }
 
@@ -34,8 +32,9 @@ const procResponse = (msg, res) => {
  * @param {Object} req Express req object
  * @param {Object} res Express res object
  * @param {String} lambdas Path to the lambdas directory
+ * @param {String} output Output destination
  */
-const runLambda = (req, res, lambdas) => {
+const runLambda = (req, res, lambdas, output) => {
   const evt = req.body
   const lambda = req.params.endpoint
   // Custom stuff...
@@ -45,18 +44,19 @@ const runLambda = (req, res, lambdas) => {
   // Execute lambda
   const proc = fork(runner, [ lambda ], { env: { lambdas, event } })
   // Print pid
-  console.log(`PID ${proc.pid} running ${lambda}`)
+  procResponse({ type: 'metric', output: `PID ${proc.pid} running ${lambda}` })
   // Await proc
-  proc.on('message', (msg) => procResponse(msg, res))
+  proc.on('message', (msg) => procResponse(msg, res, output))
 }
 
 /**
  * Core app method, binds endpoints and starts listener
  * @param {String} lambdas Path to the lambdas directory
+ * @param {String} output Designates output type `console` or `response`
  */
-export const app = (lambdas) => {
-  service.all('/api/:endpoint', (req, res) => runLambda(req, res, lambdas))
+export const app = (lambdas, output = 'console') => {
+  service.all('/api/:endpoint', (req, res) => runLambda(req, res, lambdas, output))
   service.listen(port, () => {
-    console.log(`Service running on ${port}`)
+    log.info(`Service running on ${port}`)
   })
 }
